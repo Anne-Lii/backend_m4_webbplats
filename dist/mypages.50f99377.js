4bfd9c78a5833543af287acd8fea781c7bd586f4
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-05 19:30:10 +0200

    npm run build

---
+++ dist/mypages.50f99377.js
@@ -0,0 +1,2 @@
+async function o(){localStorage.getItem("token")||(window.location.href="index.html")}window.onload=o;
+//# sourceMappingURL=mypages.50f99377.js.map
---
+++ dist/mypages.c01c269c.js
@@ -0,0 +1,2 @@
+!function(){async function n(){localStorage.getItem("token")||(window.location.href="index.html")}window.onload=n}();
+//# sourceMappingURL=mypages.c01c269c.js.map
